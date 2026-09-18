3bd920136fae290dd8bf014eb96014af69453bd8
Eugén Cowie <eugencowie@users.noreply.github.com>
2026-09-18 20:54:33 +0100

    Add avatar to home page

--- tests/e2e/index.test.ts
+++ tests/e2e/index.test.ts
@@ -3,6 +3,6 @@ import { describe, expect, it } from "./playwright";
 describe("Index", () => {
   it("has the correct title", async ({ page }) => {
     await page.goto("/");
-    await expect(page).toHaveTitle("Astro");
+    await expect(page).toHaveTitle("eugen codes");
   });
 });
